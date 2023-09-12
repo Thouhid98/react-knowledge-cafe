c76c024da1f3d1f2af889a52d9846ba16d493153
Thouhid98 <thouhidhussain98@gmail.com>
2023-09-12 10:27:36 +0600

    React Core Project: Cafe

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,11 @@
+// import PropTypes from 'prop-types';
+
+const Bookmarks = () => {
+    return (
+        <div className="md:w-1/3">
+            <h2>Bookmarks</h2>
+        </div>
+    );
+};
+
+export default Bookmarks;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import profile from '../../assets/images/profile.png';
+const Header = () => {
+    return (
+        <header className='flex justify-between p-4 mx-4'>
+            <h1 className='text-3xl text-center font-bold '>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+        </header>
+    );
+};
+
+export default Header;
